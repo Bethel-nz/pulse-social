4f06ca5345d90535d1acd45bfb696f52c2035360
Nzekea Bethel <nzekeabethel@outlook.com>
2023-10-13 16:52:22 +0100

    feat: added the session provider context to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,45 @@
 // These styles apply to every route in the application
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import AuthStatus from "@/components/auth-status";
-import { Suspense } from "react";
+import '@/styles/globals.css';
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+import AuthStatus from '@/components/auth-status';
+import AuthProvider from './context/AuthProvider';
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+	variable: '--font-inter',
+	subsets: ['latin'],
 });
 
-const title = "Next.js Prisma Postgres Auth Starter";
+const title = 'Pulse Social App';
 const description =
-  "This is a Next.js starter kit that uses Next-Auth for simple email + password login and a Postgres database to persist the data.";
+	'A Socail interaction app where intermediate and beginner devs can share thier progress and bugs, while still taking on challenges';
 
 export const metadata: Metadata = {
-  title,
-  description,
-  twitter: {
-    card: "summary_large_image",
-    title,
-    description,
-  },
-  metadataBase: new URL("https://nextjs-postgres-auth.vercel.app"),
-  themeColor: "#FFF",
+	title,
+	description,
+	twitter: {
+		card: 'summary_large_image',
+		title,
+		description,
+	},
+	metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
+	themeColor: '#FFF',
 };
 
 export default async function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.variable}>
-        <Toaster />
-        <Suspense fallback="Loading...">
-          {/* @ts-expect-error Async Server Component */}
-          <AuthStatus />
-        </Suspense>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={inter.variable}>
+				<Toaster />
+				<AuthProvider>{children}</AuthProvider>
+				<AuthStatus />
+				{children}
+			</body>
+		</html>
+	);
 }
