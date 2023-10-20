4803a157bf06b900e4d1ecbc043a9fc0a194d44d
Nzekea Bethel <nzekeabethel@outlook.com>
2023-10-20 10:31:55 +0100

    fix: added support for svg

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     domains: ['res.cloudinary.com']
   },
   swcMinify: true,
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
