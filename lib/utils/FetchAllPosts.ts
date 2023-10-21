213fd8a1b48a4c910a4721cbef2c6414e77c25a5
Nzekea Bethel <nzekeabethel@outlook.com>
2023-10-21 11:21:09 +0100

    removed next:revalidate option

--- lib/utils/FetchAllPosts.ts
+++ lib/utils/FetchAllPosts.ts
@@ -2,9 +2,7 @@ import { Post } from '@/types/type';
 import { BASE_URL } from '../lib';
 
 export const fetchAllPost = async () => {
-	const response = await fetch(`http://${BASE_URL}/api/post`, {
-		next: { revalidate: 600 },
-	});
+	const response = await fetch(`http://${BASE_URL}/api/post`);
 	const data = await response.json();
 	console.log(data);
 	return data as Promise<Post[]>;
