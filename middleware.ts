70b651c34b855058089b3d8f0d5d54c55e6398b0
Nzekea Bethel <nzekeabethel@outlook.com>
2023-10-21 08:43:38 +0100

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,21 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/protected', '/home'] };
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import type { NextRequest, NextResponse as Response } from 'next/server';
+import { authOptions } from './app/api/auth/[...nextauth]/route';
+
+export async function middleware(request: NextRequest, response: Response) {
+	const session = await getServerSession(authOptions);
+	if (!session) return NextResponse.redirect('/login');
+}
+
+export const config = {
+	matcher: [
+		'/protected',
+		'/home',
+		'/profile',
+		'/post/:path*',
+		'/api/:function*',
+	],
+};
