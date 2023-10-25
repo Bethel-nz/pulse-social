d12298006a275a222794b32ff707c484e8bf7802
Nzekea Bethel <nzekeabethel@outlook.com>
2023-10-25 06:34:34 +0100

    fix: signout page

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -4,14 +4,17 @@ import { useRouter } from 'next/navigation';
 
 export default function SignOut() {
 	const router = useRouter();
+	const SignOut = ()=> {
+	signOut();
+		setTimeout(() => {
+			console.log('done');
+			router.push('/login');
+		}, 850);
+	}
 	return (
 		<button
 			className='text-stone-400 hover:text-stone-200 transition-all border-2 p-4 rounded-md'
-			onClick={() => {
-				signOut();
-				router.replace('/login');
-			}}
-		>
+			onClick={SignOut()}>
 			Goddammit, sign me out!
 		</button>
 	);
